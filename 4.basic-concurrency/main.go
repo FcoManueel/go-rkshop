92bebd48222c4138e25775ed1df379f27da3fbf7
Manuel Valle <FcoManueel@gmail.com>
2016-04-14 12:55:17 -0700

    Add basic concurrency exercise

---
+++ 4.basic-concurrency/main.go
@@ -0,0 +1,20 @@
+package main
+
+import(
+    "fmt"
+    "time"
+)
+
+func main() {
+    fmt.Println("start")
+    boring("boring!")
+    //    go boring("async boring!")
+    fmt.Println("end")
+}
+
+func boring(msg string) {
+    for i := 0; ; i++ {
+        fmt.Println(msg, i)
+        time.Sleep (2 * time.Second)
+    }
+}
